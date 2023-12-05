36004ea2163ad69289455f9d824228ee408c34ab
Dimitrije Stevanovic <dimitrije12012007@gmail.com>
2023-12-05 13:47:08 +0100

    Add project files.

---
+++ Skocko/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Skocko
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Skocko/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Skocko
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
